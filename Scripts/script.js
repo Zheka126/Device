4b0f40d318722bda78f83b5db6ad37987a29c222
Zheka126 <62949851+Zheka126@users.noreply.github.com>
2020-07-10 12:15:10 +0300

    I don't want to do this anymore

--- Scripts/script.js
+++
@@ -1,9 +0,0 @@
-var brend_image = document.querySelector(".brend_item");
-var brend_image_color = document.querySelector(".brend_item_color");
-
-
-brend_image.addEventListener("hover", function (evt) {
-    evt.preventDefault(); 
-    brend_image.classList.add("visually_hidden");
-
-}); 
